b261f7f3113fb064a7b0e6cce572672d0ed2b224
vvede <v.vedeshenkov@gmail.com>
2024-03-06 18:36:11 -0800

    Initial commit

---
+++ src/test/java/com/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/step_defs",
+        dryRun = false,
+        tags ="@"
+
+)
+public class CukesRunner {
+
+}
---
+++ src/test/java/com/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/step_defs"
+)
+
+public class FailedTestRunner {
+}
